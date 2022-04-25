2dc4fac5f5558be354963a5ccf74deac9db52559
Emma Gunnari <emma.gunnari@iths.se>
2022-04-25 13:02:40 +0200

    added http req commit

--- Ocelat/Program.cs
+++ Ocelat/Program.cs
@@ -1,31 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+namespace Ocelat;
 
-namespace Ocelot.Demo
+public static class Program
 {
-    public static class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-                    webBuilder.UseStartup<Startup>();
-                    webBuilder.ConfigureAppConfiguration(config =>
-                        config.AddJsonFile($"ocelot.{env}.json"));
-                })
-            .ConfigureLogging(logging => logging.AddConsole());
-      
+        CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                webBuilder.UseStartup<Startup>();
+                webBuilder.ConfigureAppConfiguration(config =>
+                    config.AddJsonFile($"ocelot.{env}.json"));
+            });
 }
--- Ocelat/Startup.cs
+++ Ocelat/Startup.cs
@@ -1,34 +1,30 @@
 ﻿using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
-namespace Ocelot.Demo
+namespace Ocelat;
+
+public class Startup
 {
-    public class Startup
+    public void ConfigureServices(IServiceCollection services)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        services.AddOcelot();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
         {
-            services.AddOcelot();
+            app.UseDeveloperExceptionPage();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            endpoints.MapControllers();
+        });
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-
-                endpoints.MapControllers();
-            });
-
-            await app.UseOcelot();
-        }
+        await app.UseOcelot();
     }
 }
